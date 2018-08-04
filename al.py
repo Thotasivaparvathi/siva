7c33c32581a10a6352175e4a3829d7eff52a7a58
Thotasivaparvathi <41770262+Thotasivaparvathi@users.noreply.github.com>
2018-08-04 15:33:59 +0530

    Update al.py

--- al.py
+++ al.py
@@ -1,10 +1,8 @@
 ch=input()
-while(ch>='a' and ch<='z') or (ch>='A' and ch<='Z'):
-            if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'
-            or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+if (ch>='a' and ch<='z') or (ch>='A' and ch<='Z'):
+            if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
                          print("Vowel")
             else:
                          print("Consonant")
-            break;
-if(ch<'a' and ch<'A'):
+else:
             print("invalid")
