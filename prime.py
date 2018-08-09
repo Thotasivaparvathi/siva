c0b0f73333c83e1c89910ee838b2f0a6d29be2ac
Thotasivaparvathi <41770262+Thotasivaparvathi@users.noreply.github.com>
2018-08-09 13:07:51 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,12 +1,9 @@
 N=int(input())
 k=0
-if(N<=1000):
-            for i in range(2,N//2+1):
-                        if(N%i==0):
-                                    k=k+1
-            if(k<=0):
-                        print("Yes")
-            else:
-                        print("no")
+for i in range(2,N//2+1):
+            if(N%i==0):
+                        k=k+1
+if(k<=0):
+            print("Yes")
 else:
-                        print("no")
+            print("no")
