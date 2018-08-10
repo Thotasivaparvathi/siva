45babb2bd9d8e85b537cae3e47505616dddc31e8
Thotasivaparvathi <41770262+Thotasivaparvathi@users.noreply.github.com>
2018-08-10 14:57:31 +0530

    Create program19.py

---
+++ program19.py
@@ -0,0 +1,12 @@
+num=int(input())
+factorial=1
+if(num>0):
+   for i in range(1,num+1):
+       factorial=factorial*i
+   print(factorial)
+elif(num==0):
+          print("1")
+else:
+          print("sorry")
+   
+    
