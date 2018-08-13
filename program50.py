2c53a6bbd0b40035428fc45167934c3df8ce10ae
Thotasivaparvathi <41770262+Thotasivaparvathi@users.noreply.github.com>
2018-08-13 16:32:57 +0530

    Update program50.py

--- program50.py
+++ program50.py
@@ -1,7 +1,7 @@
 N=int(input())
 for i in range(1,N+1):
-          	if(N==2**i):
-            	          print("yes")
-            	          break
+            if(N==2**i):
+            	         print("yes")
+            	         break
 else:
-	          print("no")   
+	    print("no")   
