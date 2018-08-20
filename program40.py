e572dc110b88934447c95ce93facfd5a2d952e7d
Thotasivaparvathi <41770262+Thotasivaparvathi@users.noreply.github.com>
2018-08-20 22:12:01 +0530

    Update program40.py

--- program40.py
+++ program40.py
@@ -1,15 +1,15 @@
-n=int(input())
+r=int(input())
 f1=1
 f2=1
 i=2
-if(n<=0):
+if(r<=0):
 	print("positive number")
-elif(n==1):
+elif(r==1):
 	print(f1)
 else:
 	print (f1, end=' ')
 	print (f2, end=' ')
-	while(i<n):
+	while(i<r):
 		f3=f1+f2
 		print (f3, end=' ')
 		f1=f2
