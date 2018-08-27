3252884e7112033d358913bcdb10a4a7716f4096
Thotasivaparvathi <41770262+Thotasivaparvathi@users.noreply.github.com>
2018-08-27 12:49:17 +0530

    Create program57.py

---
+++ program57.py
@@ -0,0 +1,10 @@
+N,K=map(int,input().split())
+list=[int(x) for x in input().split()]
+if N in list:
+	count=list.count(N)
+	print(count)
+elif K in list:
+	count=list.count(K)
+	print(count)
+else:
+	print("no")
