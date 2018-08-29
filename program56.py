6051a080ef5d79b72a13bcc178e35513fa2a44aa
Thotasivaparvathi <41770262+Thotasivaparvathi@users.noreply.github.com>
2018-08-29 12:29:30 +0530

    Create program56.py

---
+++ program56.py
@@ -0,0 +1,6 @@
+S=input()
+A=list(S)
+if ("1" in A)or("2" in A)or("3" in A)or("4" in A)or("5" in A)or("6" in A)or("7" in A)or("8" in A)or("9" in A)or("0" in A):
+    print("Yes")
+else:
+    print("No")
